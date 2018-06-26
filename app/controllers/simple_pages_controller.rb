9fb2086b1de3f400b0940b0c962b9b44d2661d2d
patrickmcolletta <pc@patrickcolletta.com>
2018-06-26 04:48:26 +0000

    added several pages and expanded the store idea, still dont have an idea for a niche to create the site around.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,9 +2,18 @@ class SimplePagesController < ApplicationController
   def home
   end
   
+  def shop
+  end
+  
+  def blog
+  end
+  
   def about
   end
 
   def contact
+  end
+  
+  def login
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
+  
+  get 'simple_pages/shop'
+  
+  get 'simple_pages/blog'
+  
+  get 'simple_pages/login'
+
 
   #get 'simple_pages/index'
 
